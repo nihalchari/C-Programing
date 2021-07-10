0b35328999c1521dd83ad27e81b4cb3b89d989d8
Nihal Chari <30363800+nihalchari@users.noreply.github.com>
2021-07-11 01:41:25 +0530

    Update predefined_range_float_double.c

--- predefined_range_float_double.c
+++ predefined_range_float_double.c
@@ -14,6 +14,24 @@ int main(void) {
     printf("DBL_MIN     :   %g\n", (double) DBL_MIN);
     printf("-DBL_MAX     :  %g\n", (double) -DBL_MAX);
     printf("Precision value: %d\n", FLT_DIG );
+    
+    
+  float f_max = FLT_MAX;
+  printf("%.7f\n",f_max);
+  ++f_max;
+  printf("%.7f\n",f_max);
+  
+  float f_min = FLT_MIN;
+  printf("%.7f\n",f_min);
+  
+  double d_max = DBL_MAX;
+  printf("%.15f\n",d_max);
+  ++d_max;
+  printf("%.15f\n",d_max);
+    
+  
+  double d_min = DBL_MIN;
+  printf("%.15f\n",d_min);
 
     return 0;
 }
