7ee665cb43fb0b82b7125b297abb0d86742a3232
Nihal Chari <30363800+nihalchari@users.noreply.github.com>
2021-07-11 00:58:40 +0530

    Update float_double.c

--- float_double.c
+++ float_double.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 int main(){
+    
+    printf("size of float data %lu\n", sizeof(float));
+    printf("size of double data %lu\n", sizeof(double));
+    printf("size of long double data %lu\n", sizeof(long double));
+    
+    
+    #if 0
     int i = 960;
     float f = i*0.001f ;
     double d = i*0.001 ;
-
     
     printf("%.7f\n", f);
     printf("%.15lf\n", d);
+    #endif 
     
     return 0;
 } 
