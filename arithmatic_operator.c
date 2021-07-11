a037962e8e1d3daf3f98027a9fe8ea4550420728
Nihal Chari <30363800+nihalchari@users.noreply.github.com>
2021-07-11 19:17:22 +0530

    Create arithmatic_operator.c

---
+++ arithmatic_operator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 5;
+    int y = 5;
+    
+    
+    //#if 0
+    int add = x+y;
+    int sub = x-y;
+    int mul = x*y;
+    int divd = x/y;
+    int mod = x%y;
+    
+    printf("%d\n",add);
+    printf("%d\n",sub);
+    printf("%d\n",mul);
+    printf("%d\n",divd);
+    printf("%d\n",mod);
+    //#endif
+    
+    
+    float a = 4.2f;
+    float b = 2.1f;
+    
+    
+    float fadd = a+b;
+    float fsub = a-b;
+    float fmul = a*b;
+    float fdiv = a/b;
+  //  float mdiv = a%b;
+    
+    
+    
+    
+    
+    return 0;
+}
