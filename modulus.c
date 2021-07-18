83b4bb420f18d27c5cedad496358baa72e435ce6
Nihal Chari <30363800+nihalchari@users.noreply.github.com>
2021-07-18 18:52:51 +0530

    Create modulus.c

---
+++ modulus.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int x=0;
+    
+    
+    //if N < D, then N%D is N
+    //if N is -ve, then sign of ans will always be -ve
+    
+    
+    printf("%d\n", 3%-5);
+      
+
+    return 0;
+}
