dcf4aff99bc95a32bf09ea3c54f483a748195749
Nihal Chari <30363800+nihalchari@users.noreply.github.com>
2021-08-21 21:52:09 +0530

    Update break.c

--- loop/break.c
+++ loop/break.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+
+while(1)
+{
+   printf("outer loop start..\n");
+   
+   while(1)
+   {
+       printf("inner loop start...\n");
+       break;
+       printf("inner loop end....\n");
+   }
+   //  break;
+     printf("outer loop end...\n");
+
+}
+
+    return 0;
+}
